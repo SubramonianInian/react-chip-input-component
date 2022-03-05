9e7b8acb5a265a49e1ed345ac67ff3479a406d07
Subramonian Inian <risubramonian@gmail.com>
2022-03-06 01:37:32 +0530

    Updating imports

--- src/components/chip/chip.js
+++ src/components/chip/chip.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "../chip/chip.css";
 export const Chip = ({ chips, setChips, placeholder, chipPrefix = "" }) => {
   const [value, setValue] = useState("");
